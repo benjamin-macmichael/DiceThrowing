4c4f04073961d53551f4625c5fa3c21fe8894cba
Ben Macmichael <benjaminmac21@gmail.com>
2024-01-16 17:57:39 -0700

    comments

--- Dice.cs
+++ Dice.cs
@@ -8,19 +8,25 @@ namespace DiceThrowing
 {
     internal class Dice
     {
+        //receive the number of rolls as a parameter for this method
         public static int[] DiceRoll(int NumberOfRolls) 
         {
+            //create an instance of the system's random class that we'll use below to roll the dice
             Random random = new Random();
 
+            //the array that keeps track of the roll outcomes
             int[] output = new int[11];
 
+            //this loop rolls the 2 dice however many times the user input to roll
             for (int iCount = 0; iCount < NumberOfRolls; iCount++) 
             {
+                //rolls each die
                 int die1 = random.Next(1, 7);
                 int die2 = random.Next(1, 7);
 
                 int total = die1 + die2;
 
+                //adjusts the array accordingly, we use -2 since position 0 in the array is 2 and position 10 is 12
                 output[total - 2] = output[total - 2] + 1;
             }
 
--- Program.cs
+++ Program.cs
@@ -4,18 +4,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // Greeting/Prompt
         System.Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
 
+        //Loop that checks if user input an integer
         while (true)
-        {
+        {   
+            //If user input a valid integer, then it gets assigned to a variable
             if (int.TryParse(System.Console.ReadLine(), out int NumberOfRolls))
-            {
+            {   
+                //pass the Number of rolls to the method in our Dice class and assign what is returned to output
                 int[] output = Dice.DiceRoll(NumberOfRolls);
 
+                //pass the output and the initial number of rolls to the PrintHist method
                 PrintHist(output, NumberOfRolls);
 
                 break;
             }
+            //If not a number, the system lets the user know and gives them another chance to enter an integer
             else
             {
                 System.Console.Write("Invalid input. Enter an integer.");
@@ -23,6 +29,7 @@ internal class Program
         }
     }
 
+    //This is the method that shows the user the histogram and stats associated with the number of rolls they chose
     private static void PrintHist(int[] output, int NumberOfRolls)
     {
         System.Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = {NumberOfRolls}.\n");
